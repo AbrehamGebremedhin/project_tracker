25c021d16b7a6d0c5372e1821acc855f7a47bcae
Abreham <abreham.gmedhin12@gmail.com>
2023-10-16 21:27:22 +0300

    Added a percentage of completion for projects

--- project/serializers.py
+++ project/serializers.py
@@ -6,6 +6,21 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     tasks = TaskSerializer(many=True, read_only=True)
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        total = 0
+        completed = 0
+        for task in instance.tasks.all():
+            total += 1
+            if task.status:
+                completed += 1
+
+        if completed == 0:
+            data['Completed_Percentage'] = 0
+        else:
+            data['Completed_Percentage'] = (completed / total)*100
+        return data
+
     class Meta:
         model = Project
         fields = (
